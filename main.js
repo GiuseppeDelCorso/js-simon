5d4b19e23d5bfef5c1f98364df9ceda947c8a257
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-09 17:39:52 +0100

    sv

---
+++ main.js
@@ -0,0 +1,33 @@
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const numeriCasuali = []
+for (let i = 0; i < 5; i++) {
+    
+    const contenitoreLista = document.createElement("div")
+    const listaNumeri = document.getElementById("ListaNumeri")
+    const numeriCasuali = getRndInteger(1, 100)
+    numeriCasuali[i] = getRndInteger(1, 100)
+    listaNumeri.appendChild(contenitoreLista)
+    console.log(numeriCasuali)
+    contenitoreLista.innerHTML = numeriCasuali
+    
+    
+}
+
+setTimeout (funzioneTempo, 2*1000);
+
+
+function funzioneTempo(){
+
+    const numeriDellUtente = parseInt(prompt("Inserisci numeri"))
+   
+    console.log(numeriDellUtente)
+    if (numeriCasuali.includes(numeriDellUtente)) {
+    
+        console.log("yes")
+        
+    }
+    
+}
