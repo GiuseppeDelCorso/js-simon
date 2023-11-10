100a2536a8a387e9c3c6c65c17caa6fe7b3ce326
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-10 10:23:40 +0100

    salvataggio

--- main.js
+++ main.js
@@ -22,14 +22,51 @@ setTimeout (funzioneTempo, 2*1000);
 function funzioneTempo(){
 
     const numeriDellUtente = parseInt(prompt("Inserisci numeri"))
-   
-    console.log(numeriDellUtente)
+    const numeriDellUtente1 = parseInt(prompt("Inserisci numeri"))   
+    const numeriDellUtente2 = parseInt(prompt("Inserisci numeri"))
+    const numeriDellUtente3 = parseInt(prompt("Inserisci numeri"))
+    const numeriDellUtente4 = parseInt(prompt("Inserisci numeri"))
 
+    console.log(numeriDellUtente)
+    console.log(numeriDellUtente1)
+    console.log(numeriDellUtente2)
+    console.log(numeriDellUtente3)
+    console.log(numeriDellUtente4)
 
     if (numeriCasuali.includes(numeriDellUtente)) {
     
         console.log("yes")
         
+    }else {
+        console.log("no")
+    }
+    if (numeriCasuali.includes(numeriDellUtente1)) {
+    
+        console.log("yes")
+        
+    }else {
+        console.log("no")
+    }
+    if (numeriCasuali.includes(numeriDellUtente2)) {
+    
+        console.log("yes")
+        
+    }else {
+        console.log("no")
+    }
+    if (numeriCasuali.includes(numeriDellUtente3)) {
+    
+        console.log("yes")
+        
+    }else {
+        console.log("no")
+    }
+    if (numeriCasuali.includes(numeriDellUtente4)) {
+    
+        console.log("yes")
+        
+    }else {
+        console.log("no")
     }
     
 }
